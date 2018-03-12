4431484aab4ec37a2d66fda02f86814aa1b7c948
LuoGitHub <1345085119@qq.com>
2018-03-12 12:05:14 +0800

    advertising

---
+++ Advertising/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Advertising
+//
+//  Created by apple on 16/12/17.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
